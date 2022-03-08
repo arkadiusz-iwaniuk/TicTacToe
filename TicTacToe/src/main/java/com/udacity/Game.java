f662f7ae8c0b5f1bd59b06954fcc3cf19a5958ea
arkadiusz-iwaniuk <aiwaniuk86@gmail.com>
2022-03-08 15:49:34 +0100

    remove "none"

--- TicTacToe/src/main/java/com/udacity/Game.java
+++ TicTacToe/src/main/java/com/udacity/Game.java
@@ -179,7 +179,6 @@ public class Game {
         else if(grid[0][0] != '-' && grid[0][1] != '-' && grid[0][2] != '-' && grid[1][0] != '-' && grid[1][1] != '-' && grid[1][2] != '-' && grid[2][0] != '-' && grid[2][1] != '-' && grid[2][2] != '-'){
             result = "tie";
         }
-        else result = "none";
         return result;
     }
 
